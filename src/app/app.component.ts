608d320cb29975a79fda78d56a84f300fc58a9b7
MEzequielFB <ezequiel_frisone@hotmail.com>
2022-07-28 10:50:08 -0300

    Agrega formulario de registro usando ngModule y ngIf. También instala bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'directivasApp';
+
+  titulo:string;
+  nombre:string;
+  apellido:string;
+
+  registrado:boolean;
+
+  constructor() {
+
+    this.titulo = "DirectivasApp";
+    this.nombre = "";
+    this.apellido = "";
+
+    this.registrado = false;
+  }
+
+  registrarUsuario() {
+
+    if (this.nombre != "" && this.apellido != "") {
+      this.registrado = true;
+    }
+  }
+
+  setRegistrado(valor:boolean) {
+    this.registrado = valor;
+  }
 }
