eecca344543b9a5b73a4fb5f9bd43888dc24cfcd
MEzequielFB <ezequiel_frisone@hotmail.com>
2022-08-02 11:00:38 -0300

    Declara atributo de tipo array y muestra los usuarios dentro del mismo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,8 @@ export class AppComponent {
 
   registrado:boolean;
 
+  usuarios:Array<{nombre:string, apellido:string}>; //Atributo de tipo array. Se deben declarar sus atributos y el tipo de estos
+
   constructor() { //Da valores a los atributos
 
     this.titulo = "DirectivasApp";
@@ -21,6 +23,21 @@ export class AppComponent {
     this.apellido = "";
 
     this.registrado = false;
+
+    this.usuarios = [
+      {
+        nombre: "Juan",
+        apellido: "Bautista"
+      },
+      {
+        nombre: "Fernando",
+        apellido: "Fernández"
+      },
+      {
+        nombre: "Matías",
+        apellido: "Colmena"
+      }
+    ]
   }
 
   registrarUsuario() { //Registra al usuario si los valores de 'nombre' y 'apellido' no son vacíos
